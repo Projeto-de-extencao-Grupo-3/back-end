b79b968fecbd6474881a9a693c6003bb7359caad
Erick3421 <erick.miyagi@sptech.school>
2026-03-17 22:46:47 -0300

    feat: adicionando endpoint do python para centralizar no backend

---
+++ src/main/java/geo/track/service/PlateService.java
@@ -0,0 +1,35 @@
+package geo.track.service;
+
+import geo.track.dto.plate.PlateResponse;
+import geo.track.gateway.RecognizePlate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class PlateService {
+
+    @Autowired
+    private RecognizePlate recognizePlate;
+
+    public PlateResponse reconhecerPlaca(MultipartFile file) {
+        try {
+            PlateResponse response = recognizePlate.reconhecer(file);
+
+            if (response != null && response.getData() != null) {
+                String placaOriginal = response.getData().getPlate();
+
+                if (placaOriginal != null) {
+                    String placaLimpa = placaOriginal.replace("-", "").replace(" ", "").toUpperCase();
+
+                    response.getData().setPlate(placaLimpa);
+                }
+            }
+            return response;
+
+        } catch (Exception e) {
+            System.err.println("Erro ao chamar gateway: " + e.getMessage());
+            return null;
+        }
+    }
+}
