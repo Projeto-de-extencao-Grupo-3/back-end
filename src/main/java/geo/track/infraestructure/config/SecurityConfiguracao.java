df26c808bad0b045c70251cdad9bb60f74f84ba0
Erick3421 <erick.miyagi@sptech.school>
2026-04-29 19:41:25 -0300

    update: cors frontend

--- src/main/java/geo/track/infraestructure/config/SecurityConfiguracao.java
+++ src/main/java/geo/track/infraestructure/config/SecurityConfiguracao.java
@@ -2,6 +2,7 @@ package geo.track.infraestructure.config;
 
 import geo.track.infraestructure.auth.AutenticacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -110,12 +111,15 @@ public class SecurityConfiguracao {
         return new BCryptPasswordEncoder();
     }
 
+    @Value("url.frontend")
+    private String urlFrontend;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuracao = new CorsConfiguration();
 
         // Temporary: allow requests from any origin while frontend environments are being stabilized.
-        configuracao.addAllowedOriginPattern("*");
+        configuracao.addAllowedOriginPattern(urlFrontend);
 
         configuracao.setAllowedMethods(
                 Arrays.asList(
