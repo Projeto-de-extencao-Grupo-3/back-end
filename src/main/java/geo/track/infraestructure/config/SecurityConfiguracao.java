69ce44835d36512ff006d9d0fb1abea26b8d116c
Erick3421 <erick.miyagi@sptech.school>
2026-05-01 13:43:38 -0300

    testes

--- src/main/java/geo/track/infraestructure/config/SecurityConfiguracao.java
+++ src/main/java/geo/track/infraestructure/config/SecurityConfiguracao.java
@@ -110,7 +110,7 @@ public class SecurityConfiguracao {
         return new BCryptPasswordEncoder();
     }
 
-    @Value("{url.frontend}")
+    @Value("${url.frontend}")
     private String urlFrontend;
 
     @Bean
